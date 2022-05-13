f45ed6806fe2d8960e81405089a714b9531172e9
Tal Roded <troded24@gmail.com>
2022-05-12 20:43:23 -0400

    starting exploratory data analysis for immigration communities research idea

---
+++ sandbox.R
@@ -0,0 +1,7 @@
+## sandbox.R
+## Play around with IPUMS international data and see if my immigrant communities idea has potential
+## Requires:
+## Last edited 5/12/22 by Tal Roded
+###################################################################################################
+library(tidyverse)
+library(ipumsr)
